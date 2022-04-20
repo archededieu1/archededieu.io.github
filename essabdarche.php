2b235a2211a97202553de1e9f1b4c84e2515bd62
archededieu1 <104097412+archededieu1@users.noreply.github.com>
2022-04-20 20:58:21 +0200

    Add files via upload

---
+++ essabdarche.php
@@ -0,0 +1,62 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Acueil mateta</title>
+    <link rel="stylesheet"  href=" acueil arche.css">
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+</head>
+
+<body >
+
+
+    
+        <p style=" font-size: 25px; margin-top: 20px; margin-left: 20px; border-top: none;">   Entretien avec l'Eglise ! </p>
+           
+ <br/>
+<form  action=" " method=" POST" >
+
+<label style=" color:black; " > Nom: </label> <input type="text" name="nom" required="required" placeholder=" your name" class=" inpnom" style=" margin-left: 30px;">
+
+<br/>
+<br/>
+
+<label style=" color:black; " > Prenom: </label> <input type="text" name="nom" required="required" placeholder=" your last name" style=" margin-left: 10px;" >
+
+                <br/>
+                <br/>
+
+ <label style=" color:black; " > Adresse: </label> <input type="text" name="nom" required="required" placeholder=" adress" style=" margin-left: 8px;" >
+
+ <br/>
+ <br/>
+
+ <label style=" color:black; " > Eglise:</label> <input type="text" name="nom"  required="required"placeholder="église de provenance" style=" margin-left: 20px;" >
+
+ <br/>
+ <br/>
+ <label style=" color:black; " > Tél  : </label> <input type="text" name="nom"  required="required"placeholder=" Numéro" style=" margin-left: 35px;" >
+<br/>
+ <br/>
+     <label style=" color:black; " > Sujet: </label> <select name=" choix" required="required"style=" background: rgb(240, 233, 233);border-radius: 5px;width: 179px; margin-left: 25px;" > 
+                    <option> Bâpteme</option>  <option> Enseignement </option> <option> Prière délivrance</option> 
+                     <option > Aide sociale</option>  <option> Participer au culte </option> <option> Formatiopn des serviteurs</option>
+            
+                </select>
+                <br/>
+                <br/>
+
+ <label style=" color:black; " > Photo: </label> <input type="file" name="nom"  required="required"placeholder=" photo" style=" margin-left: 24px;" >
+
+ <br/>
+ <input type="submit" value="Envoyer"  name="bte" style=" width: 120px; height:20px; background:rgb(28, 38, 92); text-align: center; border-radius: 5px; color:aliceblue; margin-left: 80px;">
+
+
+            </form>
+
+
+
+</body>
+</html>
